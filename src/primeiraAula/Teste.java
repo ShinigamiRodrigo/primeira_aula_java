833e994ef89c2ce221e83276dfab895ef545e496
ShinigamiRodrigo <alesoares.2018@hotmail.com>
2018-08-20 22:43:23 -0300

    Segunda Aula Java

--- src/primeiraAula/Teste.java
+++ src/primeiraAula/Teste.java
@@ -6,18 +6,21 @@ public class Teste {
 	
 	public static void main (String args[]) {
 		
-		//Criando variaveis em Java:
-		//int num=2, num2=5;
+		//variaveis do Laço/if
+		int num=0, num2=5, total=0;
+		Scanner ler = new Scanner(System.in);
+		
+		//variaveis do ano
+		/*
 		int idade;
 		int ano_atual;
 		int ano_nascimento;
 		Scanner ler = new Scanner(System.in);
-
-		
-		
+		*/
+	
 		//Comandos básicos de entrada e saida de dados:
 		
-		System.out.println("Digite sua idade: "); 
+		/*System.out.println("Digite sua idade: "); 
         idade = ler.nextInt(); //variavel que vai guardar a idade
         
         //Criando um objeto do tipo Calendar para guardar o ano
@@ -26,19 +29,44 @@ public class Teste {
         ano_nascimento = ano_atual - idade;
         
         System.out.printf("Você nasceu no ano de " + ano_nascimento + "\n"); // \n serve para criar outra linha.
+        */
         
         //Poderia ser feito assim
         //System.out.printf("Você nasceu no ano de " + (calendario.get(Calendar.YEAR) - ler.nextInt()) + "\n");
         //mas, é sempre bom deixar o código organizado e de fácil entendimento para futuras modificações
+        
+        //=========AULA DE LAÇOS E IF=========
+    	
+        /* Operadores Lógicos
+		 *Maior que: >
+		 *Menor que: <
+		 *Igual: ==
+		 *Maior ou igual: >=
+		 *Menor ou igual: <=
+		 *Diferente: !=
+		 *E: &&
+		 *Ou: ||
+		 * */
+		//Enquanto
+				/*while(i <= 10000) {
+					System.out.println(i);
+					i = i + 1;
+				}*/
+				
 		
         
-        //Contas básicas e comando para exibir
-		/*System.out.println(num + num2);	
-		System.out.println(num - num2);	
-		System.out.println(num / num2);	
-		System.out.println(num * num2);
-		
-		System.out.println("Texto");*/
+        
+        System.out.println("Digite um número para multiplicar por 5: "); 
+        num = ler.nextInt();
+        total = num * num2;
+        
+        if(total > 20){
+        	System.out.println("O valor " + total + " é maior que 20");
+        }else if(total == 20) {
+        	System.out.println("O valor " + total + " é igual a 20");
+        }else {
+        	System.out.println("O valor " + total + " é menor que 20");
+        }
 	}
 
 }
