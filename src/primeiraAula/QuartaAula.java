90d694f629a0777a4541e21abc89dae81835cf86
ShinigamiRodrigo <alesoares.2018@hotmail.com>
2018-08-28 17:55:58 -0300

    Case terminado

--- src/primeiraAula/QuartaAula.java
+++ src/primeiraAula/QuartaAula.java
@@ -4,20 +4,20 @@ import java.util.Scanner;
 public class QuartaAula {
 
 	public static void main(String[] args) {
-		System.out.println (soma(15,20));
+		/*System.out.println (soma(15,20));
 		System.out.println (mult(2.5,25));
 		System.out.println (div(50,2));
-		System.out.println (exp(2,3));
-
+		System.out.println (exp(2,3));*/
+		System.out.println (operacoes(45,5));
 	}
 	
-	public static int soma(int n1,int n2) {
+	/*public static int soma(int n1,int n2) {
 		int total = n1 + n2;
 		return total;		
 	}
 	
 	public static double mult(double n1, int n2) {
-		double total = n1* n2;
+		double total = n1 * n2;
 		return total;
 		
 	}
@@ -30,14 +30,35 @@ public class QuartaAula {
 	public static double exp(int n1, int n2) {
 		double total = Math.pow(n1, n2);  
 		return total;
-	}
+	}*/
 	
-	public static int operacoes(int n1, int n2, int escolha) {
-	    Scannerler = new Scanner
+	public static int operacoes(int n1, int n2) {	
+		int escolha;
+	    Scanner ler = new Scanner(System.in);
 		System.out.println("Digite 1-Soma, 2-Subtração, 3-Divisão, 4-Multiplicação"); 
         escolha = ler.nextInt();
-      
+        
+        switch(escolha){
+        	case 1: 
+        		System.out.println ("A Soma do número " + n1 + " e número " + n2 + " é igual: " + (n1+n2));
+        	break;
+        	
+        	case 2: 
+        		System.out.println ("A Subtração do número " + n1 + " e número " + n2 + " é igual: " + (n1-n2));
+        	break;
+        	
+        	case 3:
+        		System.out.println ("A Divisão do número " + n1 + " e número " + n2 + " é igual: " + (n1/n2));
+        	break;
+        	
+        	case 4: 
+        		System.out.println ("A Multiplicação do número " + n1 + " e número " + n2 + " é igual: " + (n1*n2));
+        	break;
+        	
+        	default: 
+        		System.out.println("Escolha inválida");
+        }
+		return escolha; 
+		
 	}
-	
-
 }
