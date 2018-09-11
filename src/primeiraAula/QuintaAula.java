2e31798841b73076811791a56665bb9092390344
ShinigamiRodrigo <alesoares.2018@hotmail.com>
2018-09-10 22:29:03 -0300

    Exercicios Quinta Aula

---
+++ src/primeiraAula/QuintaAula.java
@@ -0,0 +1,18 @@
+package primeiraAula;
+
+public class QuintaAula {
+
+	public static void main(String[] args) {
+	double area;
+	double pi=3.1415;
+	int raio=12;
+	area=pi*Math.pow(raio, 2);
+		System.out.println(area);
+		
+		System.out.println((99%4));
+		
+		System.out.println(50*0.5);
+		
+		System.out.println(14/1.75);
+	}
+}
